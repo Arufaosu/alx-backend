db4bc6311503effe7cb4dc5cb26f16fe281db241
root <RobinLeeHenry101@gmail.com>
2024-04-03 12:01:39 +0000

    cleaning up the code and testing again

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-""" FIFOCache: Cache with First In First Out algorithm
+"""FIFOCache: Cache with First In First Out algorithm
 """
 from base_caching import BaseCaching
 from collections import deque
 
 
 class FIFOCache(BaseCaching):
-    """ A FIFO caching system
+    """FIFO caching system
     """
 
     def __init__(self):
@@ -16,28 +16,23 @@ class FIFOCache(BaseCaching):
         self.first_in_keys = deque()
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """Add an item in the cache
         """
         if key and item:
-            # If the cache is full, Remove last item first
-            # if we are not updating an existent key
             if len(self.cache_data) == self.MAX_ITEMS \
                     and key not in self.cache_data:
                 key_to_remove = self.first_in_keys.pop()
                 self.cache_data.pop(key_to_remove)
                 print('DISCARD:', key_to_remove)
 
-            # Update cache
             self.cache_data[key] = item
 
-            # If the key is already in first_in_keys, remove it first
             if key in self.first_in_keys:
                 self.first_in_keys.remove(key)
 
-            # Push the key to the stack
             self.first_in_keys.appendleft(key)
 
     def get(self, key):
-        """ Get an item by key
+        """Get an item by key
         """
         return self.cache_data.get(key)
