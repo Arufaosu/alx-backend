20d82fa98631db315dc382acedd4a84c41ecf3df
root <RobinLeeHenry101@gmail.com>
2024-04-03 12:00:01 +0000

    didnt work so now trying new code

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python3
-"""1-fifo_cache.py"""
+""" FIFOCache: Cache with First In First Out algorithm
+"""
 from base_caching import BaseCaching
 from collections import deque
 
+
 class FIFOCache(BaseCaching):
-    """FIFOCache class"""
+    """ A FIFO caching system
+    """
 
     def __init__(self):
-        """Initialize"""
+        """Initialize the caching system
+        """
         super().__init__()
         self.first_in_keys = deque()
 
     def put(self, key, item):
-        """Add an item to the cache"""
+        """ Add an item in the cache
+        """
         if key and item:
+            # If the cache is full, Remove last item first
+            # if we are not updating an existent key
             if len(self.cache_data) == self.MAX_ITEMS \
                     and key not in self.cache_data:
                 key_to_remove = self.first_in_keys.pop()
                 self.cache_data.pop(key_to_remove)
                 print('DISCARD:', key_to_remove)
 
-                self.cache_data[key] = item
+            # Update cache
+            self.cache_data[key] = item
 
-                if key in self.first_in_keys:
-                    self.first_in_keys.remove(key)
+            # If the key is already in first_in_keys, remove it first
+            if key in self.first_in_keys:
+                self.first_in_keys.remove(key)
 
-                self.first_in_keys.appendleft(key)
-        
-        def get(self, key):
-        """Retrieve an item from the cache"""
+            # Push the key to the stack
+            self.first_in_keys.appendleft(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
         return self.cache_data.get(key)
